75f06649a79feafba02a40b36336da222ffa1421
github username <github email address>
2018-03-09 08:24:17 -0500

    Done.

--- src/components/RestaurantInput.js
+++ src/components/RestaurantInput.js
@@ -33,15 +33,15 @@ export class RestaurantInput extends Component {
     return(
       <form onSubmit={(event) => this.handleOnSubmit(event)}>
         <p>
-          <input 
-            type="text" 
-            onChange={(event) => this.handleOnNameChange(event)} 
+          <input
+            type="text"
+            onChange={(event) => this.handleOnNameChange(event)}
             placeholder="restaurant name" />
         </p>
         <p>
-          <input 
-            type="text" 
-            onChange={(event) => this.handleOnLocationChange(event)} 
+          <input
+            type="text"
+            onChange={(event) => this.handleOnLocationChange(event)}
             placeholder="location" />
         </p>
         <input type="submit" />
@@ -50,4 +50,10 @@ export class RestaurantInput extends Component {
   }
 };
 
-export const ConnectedRestaurantInput = connect(null, null)(RestaurantInput)
+const mapDispatchToProps = dispatch => {
+  return {
+    addRestaurant: restaurant => dispatch(addRestaurant(restaurant))
+  }
+}
+
+export const ConnectedRestaurantInput = connect(null, mapDispatchToProps)(RestaurantInput)
